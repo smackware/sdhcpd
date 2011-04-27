a56f1f7463be81fd5b0225ddb489e6d9ec72ddb9
Lital Natan <litaln@Lital-Natans-MacBook-Pro.local>
2011-04-27 18:41:13 +0300

    More logging additions
    Now server supports log file + specifying logfile path in the command line

--- dhcpd.py
+++ dhcpd.py
@@ -23,24 +23,30 @@ def parse_argv():
     parser = optparse.OptionParser()
     parser.add_option("-n", "--no-fork", dest="no_fork", action="store_true",
                               help="Do not fork into a daemon")
+    parser.add_option("-l", "--logfile", dest="logfile", metavar="FILE",
+                              help="Specify log filepath", default="sdhcp.log")
     return parser.parse_args()
+
+def main():
+    backends = load_backends(BACKEND_CONFIG_DIRPATH)
+
+    dhcp_server = DhcpServer("eth0", backends)
+
+    try:
+        while True:
+            dhcp_server.process_next_dhcp_packet()
+    except (KeyboardInterrupt), e:
+        for backend in backends:
+            backend.close()
 
 if __name__ == '__main__':
     (options, args) = parse_argv()
+    logging.basicConfig(level=logging.DEBUG, filename=options.logfile, filemod="a")
+    logging.debug("Starting dhcp server...")
     if not options.no_fork:
         pid = os.fork()
         if pid:
-            print "Started dhcpd, PID: %d" % (pid, )
+            logging.debug("Forked server, PID: %d", pid)
             sys.exit()
+    main()
 
-backends = load_backends(BACKEND_CONFIG_DIRPATH)
-
-logging.basicConfig(level=logging.DEBUG)
-dhcp_server = DhcpServer("eth0", backends)
-
-try:
-    while True:
-        dhcp_server.process_next_dhcp_packet()
-except (KeyboardInterrupt), e:
-    for backend in backends:
-        backend.close()
